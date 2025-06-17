ea5fa416428956af5d390abc2a9d4b01f69e21b3
mmontes11 <martin11lrx@gmail.com>
2025-06-17 09:35:18 +0000

    Update bootstrap.sh

--- scripts/bootstrap.sh
+++ scripts/bootstrap.sh
@@ -2,14 +2,14 @@
 
 set -eo pipefail
 
-# GITHUB_USER=${GITHUB_USER:-mmontes11}
-# GITHUB_REPO=${GITHUB_REPO:-k8s-management }
-# GITHUB_BRANCH=${GITHUB_BRANCH:-main}
-# GITHUB_PATH=${GITHUB_PATH:-clusters/management}
-# if [ -z "$GITHUB_TOKEN" ]; then
-#   echo "GITHUB_TOKEN environment variable must be provided"
-#   exit 1
-# fi
+GITHUB_USER=${GITHUB_USER:-mmontes11}
+GITHUB_REPO=${GITHUB_REPO:-k8s-management }
+GITHUB_BRANCH=${GITHUB_BRANCH:-main}
+GITHUB_PATH=${GITHUB_PATH:-clusters/management}
+if [ -z "$GITHUB_TOKEN" ]; then
+  echo "GITHUB_TOKEN environment variable must be provided"
+  exit 1
+fi
 
 # sealed secrets
 SECRETS_NAMESPACE=${SECRETS_NAMESPACE:-sealed-secrets}
@@ -29,12 +29,12 @@ kubectl label secret \
   --overwrite
 
 # flux
-# curl -s https://fluxcd.io/install.sh | FLUX_VERSION=${FLUX_VERSION:-2.5.0} bash -s -
-# KUBECONFIG=${KUBECONFIG:-/etc/rancher/k3s/k3s.yaml} \
-# flux bootstrap github \
-#   --owner=$GITHUB_USER \
-#   --repository=$GITHUB_REPO \
-#   --branch=$GITHUB_BRANCH \
-#   --path=$GITHUB_PATH \
-#   --personal \
-#   --private=false
+curl -s https://fluxcd.io/install.sh | FLUX_VERSION=${FLUX_VERSION:-2.5.0} bash -s -
+KUBECONFIG=${KUBECONFIG:-/etc/rancher/k3s/k3s.yaml} \
+flux bootstrap github \
+  --owner=$GITHUB_USER \
+  --repository=$GITHUB_REPO \
+  --branch=$GITHUB_BRANCH \
+  --path=$GITHUB_PATH \
+  --personal \
+  --private=false
